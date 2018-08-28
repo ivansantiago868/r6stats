d84ee72f33d66009731c1a7f05584dec3cc9f54a
Ivan Del Valle <idelvalle@grupo-link.com>
2018-08-28 10:56:01 -0500

    ajuste de usuarios

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ $config = array(
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
 	"default-platform" => "psn", //Platforms: uplay - xbl - psn
 	"default-progression" => "true",
-	"ids"=>"3ae9cf98-a8d0-48df-a67b-c7bdfd8feef3,82ef9893-8686-4b6f-bd3d-33b414e94da1,650bb26b-fbc0-44f3-9030-d51d5861e5c8,564089ee-c73e-4531-bfe6-e50fc2393f45,9ffb6a0f-5d71-4760-994e-923389ff556e,87734ae1-6f39-4235-be97-f30f5e60d457,bd38755c-8f3d-4123-bf0f-88e1dee085af" //Enabled: true - false
+	"ids"=>"a1408894-7b34-4748-a323-c5fe09684256,090c510e-e633-4322-8b5b-8900c7323a56,9fb1a2fe-7aef-4bfd-b340-9521e669d861,9cac81bf-064d-4005-8469-628b2d39c239" //Enabled: true - false
 );
 
 ?>
