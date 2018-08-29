8286acedd18d907cba255ea114f4a21877686895
Ivan Del Valle <idelvalle@grupo-link.com>
2018-08-29 09:07:42 -0500

    ajustes de aipi

--- api/config.php
+++ api/config.php
@@ -10,7 +10,7 @@ $config = array(
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
 	"default-platform" => "psn", //Platforms: uplay - xbl - psn
 	"default-progression" => "true",
-	"ids"=>"a1408894-7b34-4748-a323-c5fe09684256,090c510e-e633-4322-8b5b-8900c7323a56,9fb1a2fe-7aef-4bfd-b340-9521e669d861,9cac81bf-064d-4005-8469-628b2d39c239" //Enabled: true - false
+	"ids"=>"a1408894-7b34-4748-a323-c5fe09684256,090c510e-e633-4322-8b5b-8900c7323a56,9fb1a2fe-7aef-4bfd-b340-9521e669d861,9cac81bf-064d-4005-8469-628b2d39c239,16006dee-5891-4adc-81a9-ddc1c30c9137" //Enabled: true - false
 );
 
 ?>
